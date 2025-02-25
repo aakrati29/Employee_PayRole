a2cbeaa3e055df57c7d264eeddf9da790a190a65
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-02-25 15:36:40 +0530

    [Aakrati] add. UC6 done

--- src/main/java/com/example/employeePayRole/services/EmployeeService.java
+++ src/main/java/com/example/employeePayRole/services/EmployeeService.java
@@ -2,11 +2,13 @@ package com.example.employeePayRole.services;
 
 import com.example.employeePayRole.entity.Employee;
 import com.example.employeePayRole.repository.EmployeeRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class EmployeeService {
 
@@ -21,10 +23,12 @@ public class EmployeeService {
     }
 
     public Employee getEmployeeByID(int id){
+        log.info("Fetching employee by id{}", id);
         return employeeRepo.findById(id).orElseThrow(()->new RuntimeException("exception handle"));
     }
 
     public List<Employee> getAllEmployee(){
+        log.info("Fetching all Employee from dataBase");
         return employeeRepo.findAll();
     }
 
@@ -34,11 +38,13 @@ public class EmployeeService {
         old.setSalary(updated.getSalary());
         old.setPhoneNumber(updated.getPhoneNumber());
 
+        log.info("Employee details are updated");
         return employeeRepo.save(old);
     }
 
     public void deleteEmployeeBYId(int id){
-         employeeRepo.deleteById(id);
+        log.info("Delete employee by id{}", id);
+        employeeRepo.deleteById(id);
     }
 
 }
