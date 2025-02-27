d3c0913e6638e52db00d660ece7a0f427844987a
Aakrati Barsaiyan <aakratibarsaiyan@gmail.com>
2025-02-27 16:33:02 +0530

    [Aakrati] add. UC13 done

--- src/main/java/com/example/employeePayRole/dto/PayRoleDto.java
+++ src/main/java/com/example/employeePayRole/dto/PayRoleDto.java
@@ -1,5 +1,7 @@
 package com.example.employeePayRole.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,16 +9,24 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class PayRoleDto {
-    private Integer id;
-    private String name;
-    private String phoneNumber;
-    private Long salary;
+
+    @NotBlank(message = "Gender cannot be blank")
+    @Pattern(regexp = "^(Male|Female|Other)$", message = "Gender must be Male, Female, or Other")
     private String gender;
+
+    @JsonFormat(pattern = "dd MMM yyyy")
+    @PastOrPresent(message = "Start date must be in the past or present")
     private LocalDate startDate;
+
+    @NotBlank(message = "Note cannot be blank")
     private String note;
+
+    @NotBlank(message = "Profile picture URL cannot be blank")
     private String profilePic;
+
+    @NotBlank(message = "Department cannot be blank")
     private String department;
 }
